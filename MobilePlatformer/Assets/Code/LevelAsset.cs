4f2622caf917bf80351d589926c9a6a4152eab9e
Mads Anthony <mads_n_anthony@hotmail.com>
2017-02-22 21:58:26 +0100

    Added LevelInspector.

---
+++ MobilePlatformer/Assets/Code/LevelAsset.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[CreateAssetMenu(fileName = "Level", menuName = "Level/New level", order = 1)]
+[System.Serializable]
+public class LevelAsset :ScriptableObject {
+	public string levelName;
+
+	public int someInt;
+}
---
+++ MobilePlatformer/Assets/Code/LevelInspector.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(LevelAsset))]
+public class LevelInspector : Editor {
+	public override void OnInspectorGUI()
+	{
+		LevelAsset myTarget = (LevelAsset)target;
+		myTarget.someInt = EditorGUILayout.IntField("Experience", myTarget.someInt);
+		EditorGUILayout.LabelField("Level", myTarget.levelName);
+	}
+}
