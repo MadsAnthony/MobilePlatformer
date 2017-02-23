c7855dc7f171cf62f504df7e676fb69c5972c9c2
Mads Anthony <mads_n_anthony@hotmail.com>
2017-02-23 22:26:03 +0100

    Added a grid and the ability to draw blocks for levels.

--- MobilePlatformer/Assets/Code/LevelAsset.cs
+++ MobilePlatformer/Assets/Code/LevelAsset.cs
@@ -1,8 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 [CreateAssetMenu(fileName = "Level", menuName = "Level/New level", order = 1)]
 public class LevelAsset :ScriptableObject {
+	public List<Vector2> gridObjects = new List<Vector2>();
+
+	public Vector2 heroPos;
 	public string levelName;
 
 	public int someInt;
--- MobilePlatformer/Assets/Code/LevelInspector.cs
+++ MobilePlatformer/Assets/Code/LevelInspector.cs
@@ -4,16 +4,83 @@ using UnityEditor;
 
 [CustomEditor(typeof(LevelAsset))]
 public class LevelInspector : Editor {
+
+	private Texture2D cellTexture;
+	private Vector2 selectedIndex;
+
+	private Vector2 gridStartPos = new Vector2 (100,200);
+	private Vector2 gridSize = new Vector2 (20,30);
+	private float cellSize = 20;
+	int cellType = 0;
+
 	public override void OnInspectorGUI()
 	{
-		//DrawDefaultInspector();
+		cellTexture = AssetDatabase.LoadAssetAtPath("Assets/Textures/squareWithBorder.png", typeof(Texture2D)) as Texture2D;
 
 		LevelAsset myTarget = (LevelAsset)target;
 		myTarget.someInt = EditorGUILayout.IntField ("Experience", myTarget.someInt);
 		EditorGUILayout.LabelField ("Level", myTarget.levelName);
 
+		if (GUILayout.Button ("Clear")) {
+			myTarget.gridObjects.Clear();
+		}
+		string[] cellOptions = new string[]
+		{
+			"Hero", "Block"
+		};
+		cellType = EditorGUILayout.Popup("Label", cellType, cellOptions); 
+
+		//Event.current.mousePosition;
+		if (Event.current.type == EventType.MouseDown || Event.current.type == EventType.MouseDrag) {
+			GetClosestCell (Event.current.mousePosition);
+		}
+		if (Event.current.type == EventType.MouseDown || Event.current.type == EventType.MouseDrag) {
+			if (Event.current.button == 0) {
+				if (cellType == 0) {
+					myTarget.heroPos = selectedIndex;
+				}
+				if (cellType == 1) {
+					if (!myTarget.gridObjects.Contains (selectedIndex)) {
+						myTarget.gridObjects.Add (selectedIndex);
+					}
+				}
+			}
+			if (Event.current.button == 1) {
+				myTarget.gridObjects.Remove(selectedIndex);
+			}
+			EditorUtility.SetDirty (myTarget);
+		}
+
+		DrawGrid ();
+
 		if (GUI.changed) {
 			EditorUtility.SetDirty (myTarget);
+		}
+	}
+
+	Vector2 GetClosestCell(Vector2 mousePos) {
+		mousePos -= gridStartPos;
+		selectedIndex = new Vector2(Mathf.FloorToInt(mousePos.x/cellSize),Mathf.FloorToInt(mousePos.y/cellSize));
+		return selectedIndex;
+	}
+
+	void DrawGrid() {
+		for (int x = 0; x<gridSize.x;x++) {
+			for (int y = 0; y<gridSize.y;y++) {
+				var rect = new Rect (gridStartPos.x+x*cellSize, gridStartPos.y+y*cellSize, cellSize, cellSize);
+
+				GUI.color = Color.white;
+				foreach(Vector2 gridObject in ((LevelAsset)target).gridObjects) {
+					if (x == gridObject.x && y == gridObject.y) {
+						GUI.color = Color.black;
+						break;
+					}
+				}
+				if (x ==  ((LevelAsset)target).heroPos.x && y == ((LevelAsset)target).heroPos.y) {
+					GUI.color = Color.green;
+				}
+				GUI.DrawTexture(rect,cellTexture,ScaleMode.ScaleToFit);
+			}
 		}
 	}
 }
