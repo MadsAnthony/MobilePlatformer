62fe3f0db06c3608276291e2a49f073ea662ce15
Mads Anthony <mads_n_anthony@hotmail.com>
2017-02-27 21:35:18 +0100

    Tweaked the input controls.

--- MobilePlatformer/Assets/Code/Game/Hero.cs
+++ MobilePlatformer/Assets/Code/Game/Hero.cs
@@ -43,35 +43,66 @@ public class Hero : DynamicBody {
 		Vector3 gravityDir = new Vector3 (dir.y,-dir.x,0);
 
 		isOnGround = false;
-		Move(gravityDir*Mathf.Clamp(gravity,-maxGravity,maxGravity),() => {gravity = 0; isOnGround = true;},() => {Check(dir*speed*-movingDir,() => { if (gravity<0f) {ChangeGravity(movingDir);}});});
+
+		Move(gravityDir*Mathf.Clamp(gravity,-maxGravity,maxGravity),
+			() => {
+					if (gravity<-(maxGravity*0.5f)) {
+						Director.CameraShake();
+					}
+					gravity = 0; 
+					isOnGround = true;
+				},
+			() => {
+				Check(dir*speed*-movingDir,
+						() => {
+								if (gravity<0f) {
+										ChangeGravity(movingDir);
+									}
+								});
+				});
 	}
 
 	Vector3 mouseClickPos;
 	float threshold = 0.4f;
+	bool touchConsumed = true;
 	void TouchInput() {
-		if (Input.GetMouseButtonDown(0)) {
+		if (Input.GetMouseButtonDown(0) && touchConsumed) {
+			touchConsumed = false;
 			mouseClickPos = Input.mousePosition;
 		}
+		if (Input.GetMouseButtonUp (0)) {
+			touchConsumed = true;
+		}
 		if (Input.GetMouseButton(0)) {
-			Vector3 mouseDir = (mouseClickPos - Input.mousePosition).normalized;
-			var horizontalDotProduct = Vector3.Dot (mouseDir, dir);
-			var verticalDotProduct = Vector3.Dot (mouseDir, new Vector3(dir.y, -dir.x,dir.z));
+			Vector3 mouseDir = (mouseClickPos - Input.mousePosition);
+			var horizontalDotProduct = Vector3.Dot (mouseDir.normalized, dir);
+			var verticalDotProduct = Vector3.Dot (mouseDir.normalized, new Vector3(dir.y, -dir.x,dir.z));
+			var verticalDotProductForce = Vector3.Dot (mouseDir, new Vector3(dir.y, -dir.x,dir.z));
 
-			if (Mathf.Abs (horizontalDotProduct) > threshold) {
-				movingDir = -1*(int)Mathf.Sign(horizontalDotProduct);
+			if (Mathf.Abs (horizontalDotProduct) > threshold && !touchConsumed) {
+				int newMovingDir = -1 * (int)Mathf.Sign (horizontalDotProduct);
+				touchConsumed = newMovingDir != movingDir;
+
+				movingDir = newMovingDir;
 			}
 
-			if (verticalDotProduct < -threshold  && isOnGround) {
-				Jump ();
+			if (verticalDotProduct < -threshold  && isOnGround && !touchConsumed) {
+				Jump (Mathf.Clamp(-verticalDotProductForce,12,20));
+				touchConsumed = true;
 			}
-			if (verticalDotProduct > threshold && isOnGround) {
+			if (verticalDotProduct > threshold && isOnGround && !touchConsumed) {
 				movingDir = 0;
+				touchConsumed = true;
+			}
+			if (verticalDotProduct > threshold && !isOnGround && !touchConsumed) {
+				gravity = -Mathf.Abs(verticalDotProductForce);
+				touchConsumed = true;
 			}
 		}
 	}
 
-	void Jump() {
-		gravity = 16f;
+	void Jump(float jumpForce = 16) {
+		gravity = jumpForce;
 	}
 	void ChangeGravity(int delta) {
 		dirsIndex += delta;
