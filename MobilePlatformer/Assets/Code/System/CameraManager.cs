4975fc32ff68f85d37784535ae45d0029e50d3dd
Mads Anthony <mads_n_anthony@hotmail.com>
2017-02-27 21:34:13 +0100

    Added a CameraManager.

---
+++ MobilePlatformer/Assets/Code/System/CameraManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour {
+
+	Camera camera;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void CameraShake() {
+		camera = GameObject.Find ("Main Camera").GetComponent<Camera>();
+
+		StartCoroutine(Shake(camera));
+	}
+
+	IEnumerator Shake(Camera camera) {
+		float x = 0;
+		while (true) {
+			x += 0.4f;
+			camera.transform.eulerAngles = new Vector3 (0,0,4*Mathf.Sin(x));
+			if (x > 4) {
+				break;
+			}
+			yield return null;
+		}
+		camera.transform.eulerAngles = new Vector3 (0,0,0);
+	}
+}
